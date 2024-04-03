37561753a0a8b23482c20bfebebcf582b46a72e9
Bob Taylor <btaylor@allytechllc.com>
2024-04-03 13:34:51 -0400

    added package back in

--- lib/src/gpio_switch.dart
+++ lib/src/gpio_switch.dart
@@ -1,10 +1,8 @@
-
+import 'package:dart_periphery/dart_periphery.dart';
 import 'package:flutter/foundation.dart';
 
-
 class GpioSwitch {
-  void initGpioSwitch(){
-  debugPrint('');
+  void initGpioSwitch() {
+    debugPrint('');
   }
-
 }
